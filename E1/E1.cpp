0cac5abcfc7e8fcf18a13c7f756e5b56d1777dfc
The-Coder-Kishor <58686913+The-Coder-Kishor@users.noreply.github.com>
2021-06-25 09:09:15 +0530

    Create E1.cpp

---
+++ E1/E1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 1000;
+    int sum = 0;
+    for(int i = 1; i< n; i++) {
+        if((i%3==0)||(i%5==0)) {
+            sum = sum + i;
+        }
+    }
+    cout<<sum<<endl;
+}
